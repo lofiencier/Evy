952abae5a102db1ae10ece63878128e3680d418d
Envy <798401559@qq.com>
2018-10-05 17:21:33 +0800

    it worked

--- docs/stories/General/Iiu/index.js
+++ docs/stories/General/Iiu/index.js
@@ -4,7 +4,8 @@ import Iiu from '@components/General/Iiu'
 import readme from "./readme.md"
 
 const name='Iiu'
+//或许这里还能去改造一下
 console.log(Iiu.prototype.constructor.__docgenInfo)
 
-const component = () => (<Iiu message={text("Message")} isArray={boolean("isArray")}/>)
+const component = () => (<Iiu message={text("message")} isArray={boolean("isArray")}/>)
 export { readme,component,name }
